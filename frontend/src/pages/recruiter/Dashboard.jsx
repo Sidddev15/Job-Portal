474b33f8867a5e10e9a0fb63ff305f2a71d65598
sidddev15 <siddharthpersonal1506@gmail.com>
2025-07-25 00:08:03 +0530

    fix: included token for Recruiter

--- frontend/src/pages/recruiter/Dashboard.jsx
+++ frontend/src/pages/recruiter/Dashboard.jsx
@@ -9,7 +9,9 @@ export default function RecruiterDashboard() {
 
   useEffect(() => {
     axios
-      .get('/jobs')
+      .get('/jobs', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then((res) => {
         setJobs(res.jobs.filter((job) => job.postedBy._id === user.id));
       })
